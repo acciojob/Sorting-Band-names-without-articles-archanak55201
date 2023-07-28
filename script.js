a53866bc3871f788b255273a1af5a6534c9abd3b
archanak55201 <55021100+archanak55201@users.noreply.github.com>
2023-07-28 17:59:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+function splits(name){
+	let regex= new RegExp('^(a |an |the )','i');
+	return name.replace(regex,'').trim();
+	
+}
 
+touristSpots.sort(function(a,b){
+	return (splits(a)>splits(b)?1:-1);
+})
+console.log(touristSpots);
+let ul =document.getElementsByTagName("ul")[0];
+// console.log(ul);
+
+ul.innerHTML=touristSpots.map(item=>
+    `<li>${
+    (item)
+}</li>`).join();
